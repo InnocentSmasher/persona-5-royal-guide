d94e69ba2e5e29547617d366178b065e0ca5b53c
Chuck Hutchins <chuck@chuckhutchins.com>
2021-02-06 13:45:07 -0500

    removing excess wrapping div

--- src/pages/changelog.js
+++ src/pages/changelog.js
@@ -7,18 +7,18 @@ export default function ChangelogPage() {
         <Layout>
             <SEO title={"Changelog"}/>
             <main>
-                <div>
-                    <h2>Changelog</h2>
-                    <ul>
-                        <li>2021-02-06 &ndash; Extracted out guide to it's own domain.</li>
-                        <li>2021-01-31 &ndash; Updated layout.</li>
-                        <li>2021-01-30 &ndash; Added December, January, and February.</li>
-                        <li>2021-01-18 &ndash; Added September, October, and November.</li>
-                        <li>2021-01-12 &ndash; Added July and August.</li>
-                        <li>2021-01-10 &ndash; Added June. Added Negotiation Guide resource. Switched monthly pages to numbers instead of names.</li>
-                        <li>2021-01-09 &ndash; Initialized guide. Added resources. Added April and May.</li>
-                    </ul>
-                </div>
+                <h2>Changelog</h2>
+                <ul>
+                    <li>2021-02-06 &ndash; Extracted out guide to it's own domain.</li>
+                    <li>2021-01-31 &ndash; Updated layout.</li>
+                    <li>2021-01-30 &ndash; Added December, January, and February.</li>
+                    <li>2021-01-18 &ndash; Added September, October, and November.</li>
+                    <li>2021-01-12 &ndash; Added July and August.</li>
+                    <li>2021-01-10 &ndash; Added June. Added Negotiation Guide resource. Switched monthly pages to
+                        numbers instead of names.
+                    </li>
+                    <li>2021-01-09 &ndash; Initialized guide. Added resources. Added April and May.</li>
+                </ul>
             </main>
         </Layout>
     );
