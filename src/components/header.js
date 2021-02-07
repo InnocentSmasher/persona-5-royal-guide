8804d119531732d6235fc3885ad04ad583d61ef3
Chuck Hutchins <chuck@chuckhutchins.com>
2021-02-07 17:11:53 -0500

    removing back to link from header

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,9 @@
 import React from "react"
-import { Link } from "gatsby"
 
 export default function Header() {
     return (
         <header>
             <h1>Persona 5 Royal Guide</h1>
-            <Link to="/">Back to Guide</Link>
         </header>
     );
 }
