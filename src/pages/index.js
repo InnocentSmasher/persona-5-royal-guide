442f6972974c1c2fb29bd541521cedaffe8dd36f
Chuck Hutchins <chuck@chuckhutchins.com>
2021-02-09 08:52:01 -0500

    fixing links to proper months

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,17 +16,17 @@ export default function IndexPage() {
                 <div>
                     <h3>Months</h3>
                     <ul>
-                        <li><Link to="/guide/april">April</Link></li>
-                        <li><Link to="/guide/may">May</Link></li>
-                        <li><Link to="/guide/june">June</Link></li>
-                        <li><Link to="/guide/july">July</Link></li>
-                        <li><Link to="/guide/august">August</Link></li>
-                        <li><Link to="/guide/september">September</Link></li>
-                        <li><Link to="/guide/october">October</Link></li>
-                        <li><Link to="/guide/november">November</Link></li>
-                        <li><Link to="/guide/december">December</Link></li>
-                        <li><Link to="/guide/january">January</Link></li>
-                        <li><Link to="/guide/february">February</Link></li>
+                        <li><Link to="/guide/04">April</Link></li>
+                        <li><Link to="/guide/05">May</Link></li>
+                        <li><Link to="/guide/06">June</Link></li>
+                        <li><Link to="/guide/07">July</Link></li>
+                        <li><Link to="/guide/08">August</Link></li>
+                        <li><Link to="/guide/09">September</Link></li>
+                        <li><Link to="/guide/10">October</Link></li>
+                        <li><Link to="/guide/11">November</Link></li>
+                        <li><Link to="/guide/12">December</Link></li>
+                        <li><Link to="/guide/01">January</Link></li>
+                        <li><Link to="/guide/02">February</Link></li>
                     </ul>
                 </div>
                 <div className="hr--mini"/>
