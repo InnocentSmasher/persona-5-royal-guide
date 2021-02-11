41a6f26e2ca5461a62989aaabd4b75884eb34448
Chuck Hutchins <chuck@chuckhutchins.com>
2021-02-10 19:25:16 -0500

    adding icon links to footer along with a disclaimer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import KofiLogo from '../assets/svgs/kofi.svg';
+import GitHubLogo from '../assets/svgs/github.svg';
 
 export default function Footer() {
     return (
         <footer>
             <ul>
-                <li>&copy; {new Date().getFullYear()} Chuck Hutchins</li>
-                <li>Like this? <a href="https://ko-fi.com/innocentsmasher">Buy me a coffee.</a></li>
                 <li>Made with <span role="img" aria-label="Skull">💀</span> in Michigan.</li>
+                <li className="icon-links"><a href="https://ko-fi.com/innocentsmasher" aria-label="Buy me a coffee"><KofiLogo /></a><a href="https://github.com/InnocentSmasher/persona-5-royal-guide" aria-label="Check it out on GitHub"><GitHubLogo /></a></li>
             </ul>
+            <p className="disclaimer">This site is not affiliated with ATLUS or SEGA.</p>
         </footer>
     );
 }
